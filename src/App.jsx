c37a368b57ec8f4ff2987bcacdbcc20dedd0634f
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-08-27 13:11:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <h2>Modern Landing Page</h2>
+    </div>
+    
+  );
+}
+
