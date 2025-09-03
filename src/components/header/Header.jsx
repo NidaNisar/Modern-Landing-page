236767798403e97acc8f3ca66005300039fa0675
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-09-04 00:40:29 +0500

    completing header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-            {/* <main className='max-w-[1920px] max-auto  bg-white overflow-hidden'>
-             <h1 className='h1'> Interno</h1>
-      </main> */}
-    </div>
+    
+           <header className='sticky top-0 h-[90px] z-30 bg-white shadow-xl'>
+            <div className='container mx-auto flex flex-row  justify-between ' >
+              <a href='#'>
+                <img src='/landingpics/logo.svg' className='mt-3'/>
+              </a>
+              <nav >
+                <ul className='flex flex-row gap-6 mt-6 ' >
+                  <li  className=' hover:text-accentt'>Home</li>
+                  <li className=' hover:text-accentt'>About</li>
+                  <li className=' hover:text-accentt' >Testimonal</li>
+                  <li className=' hover:text-accentt'>Our Work</li>
+                  <li className=' hover:text-accentt'>News</li>
+                  <li className=' hover:text-accentt'>Contact</li>
+                </ul>
+              </nav>
+            </div>
+           </header>
+    
   )
 }
 
