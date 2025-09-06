1b97a697d319e8351052e792be2417ed1555b595
nidanisar118@gmail.com <nidanisar118@gmail.com>
2025-09-06 14:27:51 +0500

    completeing about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.css'
 import Hero from "./components/hero/hero";
 import Header from "./components/header/Header";
 import Steps from "./components/steps/Steps";
+import About from "./components/about/About";
 export default function App() {
   return (
     <div>
       <Header/>
      <Hero/>
      <Steps/>
+     <About/>
     </div>
     
   );
---
+++ src/components/about/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div className='container mx-auto mt-35 flex flex-col sm:flex-row gap-30'>
+            <div>
+                <img src='/landingpics/about/img.png ' className='flex justify-center h-[550px] w-[340px] sm:w-[500px]  sm:h-[626px] ml-8'/>
+            </div>
+            <div className='flex flex-col gap-8  xl:mt-15'>
+                <h1 className=' mx-auto sm:text-center text-4xl w-[300px] xl:text-6xl xl:w-[650px]'>We Create The Art Of Stylish Living Stylishly</h1>
+                <p className='  mx-auto w-[300px] xl:w-[500px]'>At our core, we blend creativity with functionality to design spaces that inspire. From concept to completion, every detail is crafted with purpose and passion. Discover a lifestyle where elegance meets everyday living.</p>
+                <div className= 'container  flex gap-9 justify-center'>
+                    <i className='ri-phone-fill text-accentt text-4xl  bg-amber-100 rounded-full p-3'></i>
+                    <div > 
+                        <p className='font-black  text:1xl xl:text-2xl'>92125567896</p>
+                        <p>Call us Anytime</p>
+                       
+                    </div>
+                     
+                </div>
+                <button className='btn btn-primary mx-auto mt-3 '>Get Free Estimation
+                            <i className='ri-arrow-right-line text-accentt'></i>
+                           </button>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
